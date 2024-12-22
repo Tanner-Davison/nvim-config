494bcc8c471b79e512e325a2c4ad8afe072fa343
Tanner Davison <Tboydavison@gmail.com>
2024-12-22 02:05:09 -0700

    saving for safety, of course.

--- lua/tannerdavison/core/keymaps.lua
+++ lua/tannerdavison/core/keymaps.lua
@@ -30,3 +30,10 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- CPP compiling
+-- Compile C++ code with <Leader>cc
+vim.keymap.set("n", "<Space>cc", "<cmd>w!<CR>:!g++ % -o %:t:r <CR>", { desc = "Compile C++ code" })
+
+-- Run the compiled executable with <Leader>cv
+vim.keymap.set("n", "<Space>cv", "<cmd>w!<CR>:!cmd /c %:t:r.exe <CR>", { desc = "Run compiled C++ code" })
--- lua/tannerdavison/plugins/indent-blankline.lua
+++ lua/tannerdavison/plugins/indent-blankline.lua
@@ -3,6 +3,26 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	main = "ibl",
 	opts = {
-		indent = { char = "➺" },
+		indent = { char = "" }, -- No character for indentation (empty arrow)
+		whitespace = {
+			highlight = { "CursorColumn", "Whitespace" },
+			remove_blankline_trail = false, -- Don't remove trailing blank lines
+		},
+		scope = { enabled = false }, -- Disable scope highlighting
 	},
+	config = function()
+		local highlight = {
+			"CursorColumn",
+			"Whitespace",
+		}
+
+		require("ibl").setup({
+			indent = { highlight = highlight, char = " " }, -- Empty character for indent
+			whitespace = {
+				highlight = highlight,
+				remove_blankline_trail = false, -- Keep trailing blank lines
+			},
+			scope = { enabled = false }, -- Disable scope
+		})
+	end,
 }

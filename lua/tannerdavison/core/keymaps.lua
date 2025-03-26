43f9874fd1ec902e7ac8885450a0082e3b0f9632
Tanner Davison <Tboydavison@gmail.com>
2025-03-26 11:13:18 -0600

    saving TODOS

--- lua/tannerdavison/core/keymaps.lua
+++ lua/tannerdavison/core/keymaps.lua
@@ -28,6 +28,12 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- keymap.set("n", "<leader>td", "o-- TODO: ", { desc = "Insert a TODO comment" })
+keymap.set("n", "<leader>td", function()
+	local date = os.date("%Y-%m-%d")
+	vim.api.nvim_put({ "-- TODO [" .. date .. "]: " }, "l", true, true)
+end, { desc = "Insert a TODO with date" })
+keymap.set("n", "<leader>tD", "O-- TODO: ", { desc = "Insert a TODO comment above" }) -- Insert a TODO comment above
 
 -- Media Query Snippets
 vim.keymap.set("n", "<leader>mq", function()

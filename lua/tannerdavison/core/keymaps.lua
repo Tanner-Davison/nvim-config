1eb9430de1980d48a673d7a0b032ddb329cc3531
Tanner Davison <Tboydavison@gmail.com>
2025-03-26 11:19:12 -0600

    added current date with TODO

--- lua/tannerdavison/core/keymaps.lua
+++ lua/tannerdavison/core/keymaps.lua
@@ -28,7 +28,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-keymap.set("n", "<leader>td", function()
+keymap.set("n", "<leader>tD", function()
 	local date = os.date("%Y-%m-%d")
 	vim.api.nvim_put({ "// TODO [" .. date .. "]: " }, "l", true, true)
 end, { desc = "Insert a TODO with date" }) -- Insert a TODO comment with a timestamp

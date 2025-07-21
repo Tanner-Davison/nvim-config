b3fd49cb3e7abe11c388be1357fcbf188e531ca3
Tanner Davison <Tboydavison@gmail.com>
2025-07-20 22:08:22 -0600

    fixed module name

--- lua/tannerdavison/plugins/null-ls.lua
+++ lua/tannerdavison/plugins/null-ls.lua
@@ -5,10 +5,10 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local none_ls = require("none-ls")
-			none_ls.setup({
+			local null_ls = require("null-ls")
+			null_ls.setup({
 				sources = {
-					none_ls.builtins.formatting.clang_format.with({
+					null_ls.builtins.formatting.clang_format.with({
 						command = vim.fn.has("win32") == 1 and "C:\\msys64\\mingw64\\bin\\clang-format.exe"
 							or "clang-format",
 					}),

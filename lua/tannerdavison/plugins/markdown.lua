6110f97918100467806de792257a354c21378ea3
Tanner Davison <Tboydavison@gmail.com>
2025-07-20 19:32:07 -0600

    fixed preview function execution time

--- lua/tannerdavison/plugins/markdown.lua
+++ lua/tannerdavison/plugins/markdown.lua
@@ -4,9 +4,7 @@ return {
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		build = "cd app && npm install",
 		config = function()
 			vim.g.mkdp_theme = "dark"
 			vim.g.mkdp_filetypes = { "markdown" }
